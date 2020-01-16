dc86f0019286dc3734d14b52c06619deb0d60c05
luna215 <paulluna0215@gmail.com>
2020-01-16 11:29:48 -0800

    Added better description of compress.hs

--- compress.sh
+++ compress.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# A script that will help us bundle our files
-# into a .zip file.
+# A script that will help bundle the necessary files,
+# that our plugin will use, into a .zip file.
+
 rm -rf first_plugin.zip
 zip -r first_plugin login.php greet_user.php
